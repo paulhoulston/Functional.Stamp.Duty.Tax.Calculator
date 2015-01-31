6bfddc87dfcd285d8de0e283a5bdfd5a6900aaf2
paul.houlston <paul.houlston@hotmail.co.uk>
2015-01-31 07:20:36 +0000

    Made first test pass

--- Functional.Stamp.Duty.Tax.Calculator/MyClass.cs
+++ Functional.Stamp.Duty.Tax.Calculator/MyClass.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-using Xunit;
+using NUnit.Framework;
 
 namespace Functional.Stamp.Duty.Tax.Calculator
 {
+	[TestFixture]
 	public class given_I_am_in_the_first_stamp_duty_tax_band
 	{
 		public class when_I_pay_nothing_for_a_house
 		{
-			[Fact]
+			public class TaxCalculator
+			{
+				public int Calculate ()
+				{
+					return 0;
+				}
+			}
+
+			[Test]
 			public void then_I_dont_pay_any_stamp_duty()
 			{
-				Assert.True(false);
+				var taxCalculator = new TaxCalculator ();
+				Assert.AreEqual(0, taxCalculator.Calculate());
 			}
 		}
 	}
