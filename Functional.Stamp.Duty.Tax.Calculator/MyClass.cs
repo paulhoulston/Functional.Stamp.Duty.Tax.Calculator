e9e42b8367253612b2722d755516a110a35a690b
paul.houlston <paul.houlston@hotmail.co.uk>
2015-01-31 07:23:51 +0000

    Added second test

--- Functional.Stamp.Duty.Tax.Calculator/MyClass.cs
+++ Functional.Stamp.Duty.Tax.Calculator/MyClass.cs
@@ -10,6 +10,13 @@ namespace Functional.Stamp.Duty.Tax.Calculator
 		{
 			public class TaxCalculator
 			{
+				private readonly int _value;
+
+				public TaxCalculator (int value)
+				{
+					_value = value;
+				}
+
 				public int Calculate ()
 				{
 					return 0;
@@ -19,9 +26,20 @@ namespace Functional.Stamp.Duty.Tax.Calculator
 			[Test]
 			public void then_I_dont_pay_any_stamp_duty()
 			{
-				var taxCalculator = new TaxCalculator ();
+				var taxCalculator = new TaxCalculator (0);
 				Assert.AreEqual(0, taxCalculator.Calculate());
 			}
+
+			public class when_I_pay_one_pound_less_than_the_next_stamp_duty_tax_band
+			{
+				[Test]
+				public void then_I_dont_pay_any_stamp_duty()
+				{
+					var taxCalculator = new TaxCalculator (125000);
+					Assert.AreEqual(0, taxCalculator.Calculate());
+				}				
+			}
+
 		}
 	}
 
