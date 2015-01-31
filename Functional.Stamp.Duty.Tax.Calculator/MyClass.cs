5089e5012e6a6559b0640fb218b645a057a19b3a
paul.houlston <paul.houlston@hotmail.co.uk>
2015-01-31 07:27:53 +0000

    Moved tax calculator out of test class

--- Functional.Stamp.Duty.Tax.Calculator/MyClass.cs
+++ Functional.Stamp.Duty.Tax.Calculator/MyClass.cs
@@ -3,26 +3,26 @@ using NUnit.Framework;
 
 namespace Functional.Stamp.Duty.Tax.Calculator
 {
+	public class TaxCalculator
+	{
+		private readonly int _value;
+
+		public TaxCalculator (int value)
+		{
+			_value = value;
+		}
+
+		public int Calculate ()
+		{
+			return 0;
+		}
+	}
+
 	[TestFixture]
 	public class given_I_am_buying_a_house
 	{
 		public class when_the_purchase_price_is_within_the_first_stamp_duty_tax_bracket
 		{
-			public class TaxCalculator
-			{
-				private readonly int _value;
-
-				public TaxCalculator (int value)
-				{
-					_value = value;
-				}
-
-				public int Calculate ()
-				{
-					return 0;
-				}
-			}
-
 			[TestCase(0)]
 			[TestCase(125000)]
 			public void then_I_dont_pay_any_stamp_duty(int housePrice)
