a0813d42b63d0335bc8f3b06248e923b5c10f899
paul.houlston <paul.houlston@hotmail.co.uk>
2015-01-31 07:12:45 +0000

    Initial commit

---
+++ Functional.Stamp.Duty.Tax.Calculator/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xunit;
+
+namespace Functional.Stamp.Duty.Tax.Calculator
+{
+	public class given_I_am_in_the_first_stamp_duty_tax_band
+	{
+		public class when_I_pay_nothing_for_a_house
+		{
+			[Fact]
+			public void then_I_dont_pay_any_stamp_duty()
+			{
+				Assert.True(false);
+			}
+		}
+	}
+
+}
+
